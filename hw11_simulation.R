b03524ec64968c94234a04425e6eb2eeb1375690
Laura Peterman <peterman.laura@uwlax.edu>
2019-05-09 10:32:42 -0500

    model_select function returns p-values of a significant covariates model

--- hw11_simulation.R
+++ hw11_simulation.R
@@ -4,3 +4,22 @@ generate_data<- function(n,p){
   foo<-list(covariates, responses)
   return(foo)
 }
+
+model_select<- function(n,p, cutoff){
+  pp<- generate_data(n,p)
+  resp<-pp[[2]]
+  covar<-pp[[1]]
+  sn<-seq(from=1, to=(p))
+  colnames(covar)=sn
+qq<-lm(resp~covar)
+zap<-summary(qq)
+zappy<-data.frame(zap$coefficients)
+sap<-cbind(sn,zappy[-1,])
+sap2<-sap$sn[sap$Pr...t..>cutoff]
+wish<-lm(resp~covar[,sap2])
+wishy<-summary(wish)
+washy<-data.frame(wishy$coefficients)
+washy<-washy[,4]
+shoe<-ifelse((length(washy)<p), washy, NA)
+return(shoe)
+}
