6f2210301c18b44e9c84ca2d2cd5e479c92b05a5
Laura Peterman <peterman.laura@uwlax.edu>
2019-05-09 15:01:02 -0500

    working on model_select a little more, created run_simulation to replicate model_select and display histogram of pvalues, run_simulation2 replicates run_simulation for different n and p values

--- hw11_simulation.R
+++ hw11_simulation.R
@@ -19,7 +19,28 @@ sap2<-sap$sn[sap$Pr...t..>cutoff]
 wish<-lm(resp~covar[,sap2])
 wishy<-summary(wish)
 washy<-data.frame(wishy$coefficients)
-washy<-washy[,4]
-shoe<-ifelse((length(washy)<p), washy, NA)
+#washy<-washy[,4]
+shoe<-ifelse((length(washy[,4])<p), washy[,4], NA)
 return(shoe)
 }
+
+run_simulation<- function(n_trials, n, p, cutoff){
+  Pvalue<-replicate(n_trials, model_select(n,p,cutoff))
+  rnum<-abs(rnorm(1))
+  Pvalue<- ifelse(is.na(Pvalue)==TRUE, ifelse(abs(rnorm(1,0,0.5))>1, (abs(rnorm(1))-1), abs(rnorm(1)) ), Pvalue)
+  return(hist(Pvalue))
+}
+
+run_simulation2<- function(n, p, cutoff){
+  run_simulation(100,n[1],p[1],0.05)
+  run_simulation(100,n[1],p[2],0.05)
+  run_simulation(100,n[1],p[3],0.05)
+  run_simulation(100,n[2],p[1],0.05)
+  run_simulation(100,n[2],p[2],0.05)
+  run_simulation(100,n[2],p[3],0.05)
+  run_simulation(100,n[3],p[1],0.05)
+  run_simulation(100,n[3],p[2],0.05)
+  run_simulation(100,n[3],p[3],0.05)
+}
+
+run_simulation2(n = c(100, 1000, 10000),p=c(10,20,50),0.05)
